88ca2b20b61b2caac45b65d9a330195cdabea9c0
Jcode6 <90939549+Jcodei6@users.noreply.github.com>
2026-06-14 21:28:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
+const CACHE_NAME = 'manager-cache-v2';
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('manager-store').then((cache) => cache.addAll([
-      './',
-      './index.html'
-    ]))
-  );
+  self.skipWaiting(); // Forces the newest version to take over immediately
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
+  // Network-first strategy: Always try to get the most updated files from the internet first
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    fetch(e.request)
+      .then((response) => {
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, clonedResponse);
+        });
+        return response;
+      })
+      .catch(() => {
+        // If offline, fallback to the cache
+        return caches.match(e.request);
+      })
   );
 });
